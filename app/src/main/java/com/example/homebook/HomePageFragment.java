facd189294657dfb236589634534c5b86ccc6e11
Omar Yousef <w0753671@myscc.ca>
2020-11-20 22:08:53 -0500

    Externalized the Strings in Strings.xml

--- app/src/main/java/com/example/homebook/HomePageFragment.java
+++ app/src/main/java/com/example/homebook/HomePageFragment.java
@@ -2,7 +2,11 @@ package com.example.homebook;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,10 +23,12 @@ public class HomePageFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Integer mParam3;
 
     public HomePageFragment() {
         // Required empty public constructor
@@ -37,11 +43,12 @@ public class HomePageFragment extends Fragment {
      * @return A new instance of fragment HomePageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomePageFragment newInstance(String param1, String param2) {
+    public static HomePageFragment newInstance(String param1, String param2, Integer param3) {
         HomePageFragment fragment = new HomePageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM3, param3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,6 +59,7 @@ public class HomePageFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getInt(ARG_PARAM3);
         }
     }
 
@@ -59,6 +67,36 @@ public class HomePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+
+
+
+        return view;
+    }
+
+    public class CustomViewPageAdapter extends FragmentPagerAdapter {
+
+        public CustomViewPageAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            //Showing the viewPager slides
+            switch (position){
+                case 0: return HousesFragment.newInstance(getString(R.string.first_title), getString(R.string.first_description), R.drawable.animals_app);
+                case 1: return HousesFragment.newInstance(getString(R.string.second_title), getString(R.string.second_description), R.drawable.destination_app);
+                case 2: return HousesFragment.newInstance(getString(R.string.third_title), getString(R.string.third_description), R.drawable.hobby_app);
+                case 3: return HousesFragment.newInstance(getString(R.string.fourth_title), getString(R.string.fourth_description), R.drawable.job_app);
+                default: return HousesFragment.newInstance("404 not found", "", R.drawable.job_app);
+            }
+        }
+
+        @Override
+        public int getCount() {
+            //Returning only our 4 values
+            return 4;
+        }
     }
 }
--- app/src/main/java/com/example/homebook/HousesFragment.java
+++ app/src/main/java/com/example/homebook/HousesFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +21,14 @@ public class HousesFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
+    private static final String ARG_PARAM4 = "param4";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private String mParam4;
+    private Integer mParam3;
 
     public HousesFragment() {
         // Required empty public constructor
@@ -37,11 +43,13 @@ public class HousesFragment extends Fragment {
      * @return A new instance of fragment HousesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HousesFragment newInstance(String param1, String param2) {
+    public static HousesFragment newInstance(String param1, String param2, Integer param3, String param4) {
         HousesFragment fragment = new HousesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM3, param3);
+        args.putString(ARG_PARAM4, param4);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,6 +60,8 @@ public class HousesFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getInt(ARG_PARAM3);
+            mParam4 = getArguments().getString(ARG_PARAM4);
         }
     }
 
@@ -59,6 +69,26 @@ public class HousesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_houses, container, false);
+        View view =  inflater.inflate(R.layout.fragment_houses, container, false);
+
+        //If the parameters are not empty. set the texts and imageView to the passed value
+        if (mParam1 != null){
+            TextView housePrice = view.findViewById(R.id.housePrice);
+            housePrice.setText(mParam1);
+        }
+        if (mParam2 != null){
+            TextView houseAddress = view.findViewById(R.id.houseAddress);
+            houseAddress.setText(mParam2);
+        }
+        if (mParam3 != null){
+            ImageView houseImage = view.findViewById(R.id.houseImage);
+            houseImage.setImageResource(mParam3);
+        }
+        if (mParam4 != null){
+            TextView houseDetails = view.findViewById(R.id.houseDetails);
+            houseDetails.setText(mParam4);
+        }
+
+        return view;
     }
 }
