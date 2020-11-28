ed5b9d9e64f1ddaad70fa1619c9630aeffe3e8fd
Omar Yousef <w0753671@myscc.ca>
2020-11-27 22:48:54 -0500

    #5 Finished styling the contact page's Layout

--- app/src/main/java/com/example/homebook/ContactPageFragment.java
+++ app/src/main/java/com/example/homebook/ContactPageFragment.java
@@ -61,6 +61,7 @@ public class ContactPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact_page, container, false);
+        return view;
     }
 }
