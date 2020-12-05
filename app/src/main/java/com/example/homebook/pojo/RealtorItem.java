66e7fde647433550b3068171a035c17b0cc35506
Omar Yousef <w0753671@myscc.ca>
2020-12-04 23:27:46 -0500

    #11 Made the Realtor ListView Pojo class

--- app/src/main/java/com/example/homebook/pojo/RealtorItem.java
+++ app/src/main/java/com/example/homebook/pojo/RealtorItem.java
@@ -1,4 +1,42 @@
 package com.example.homebook.pojo;
 
 public class RealtorItem {
+    private String name;
+    private String address;
+    private String contact;
+
+    public RealtorItem(String name, String address, String contact) {
+        this.name = name;
+        this.address = address;
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
