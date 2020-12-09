5f912c2311c19da2ac1762dff891b1359d75b550
Omar Yousef <w0753671@myscc.ca>
2020-12-08 20:33:24 -0500

    Created the SettingsActivity and the preferences.xml resource file

---
+++ app/src/main/java/com/example/homebook/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.homebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public static class MySettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
